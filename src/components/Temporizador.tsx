55d8c555c7786b4999246139d3366c78dee9889f
joaovsz <joaovitor1713coin@gmail.com>
2024-02-03 15:04:11 -0300

    types error

--- src/components/Temporizador.tsx
+++ src/components/Temporizador.tsx
@@ -16,10 +16,11 @@ export default function Temporizador(props: TemporizadorProps) {
                 size={120}
                 isPlaying
                 onComplete={props.tempoEsgotado}
+                colorsTime={[0.33, 0.33, 0.33]}
                 colors={[
-                    ['#BCE596', 0.33],
-                    ['#F7B801', 0.33],
-                    ['#ED827A', 0.33],
+                    '#BCE596',
+                    '#F7B801',
+                    '#ED827A',
                 ]}>
                 {({ remainingTime }) => remainingTime}
             </CountdownCircleTimer>
